d45fa298446c60db375ed59a82878e9ea77718d2
Maulid Bwabo <bwabomaulid@gmail.com>
2020-08-21 13:12:27 +0300

    Splash 2

--- Paper 2.R
+++ Paper 2.R
@@ -2,4 +2,5 @@
 #Obsevred Heterogeneity 
 #Product Terms Approach, Mutli-Group Analysis and Pathmox
 #Analysis of Interactions Terms, Age and Gender Effects
-
+#Working Directory
+setwd("~/GitHub/Advances Analysis/.git")
